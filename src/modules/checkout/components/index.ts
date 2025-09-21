612630194968dc74a2a2a2b8a2b8f7e53fbc5bf2
mohamadsobeh <mohammadsobh1999@gmail.com>
2025-09-22 01:55:27 +0300

    Fix OrderSummary export issue and cleanup checkout module

--- src/modules/checkout/components/index.ts
+++ src/modules/checkout/components/index.ts
@@ -1,7 +1,10 @@
 export * from './checkout-modal.component';
 export * from './table-selector.component';
 export * from './coupon-input.component';
-export { OrderSummaryComponent as OrderSummary } from './order-summary.component';
+
+// ✅ خليه باسم أصلي عشان ما يتعارض مع الـ type
+export { OrderSummaryComponent } from './order-summary.component';
+
 export * from './place-order-button.component';
 export * from './phone-collection-modal.component';
 export * from './user-details-bottom-sheet.component';
